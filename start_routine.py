ff9358a800665ab7643854531d25e3fa83519e84
Vladimir Zolotykh <vladimir.zolotykh@gmail.com>
2025-08-14 21:45:05 +0300

    Refactor RoutineEditor.__init__, add_exercise
    Fix .grid alignment

--- start_routine.py
+++ start_routine.py
@@ -16,17 +16,17 @@ class RoutineEditor(tk.Toplevel):
         self.columnconfigure(0, weight=1)
         topframe.grid(row=0, column=0, sticky=tk.NSEW)
         ts_frame = tk.Frame(topframe)
-        ts_frame.grid(row=0, column=0)
+        ts_frame.grid(row=0, column=0, sticky=tk.W)
         tk.Label(ts_frame, text="When: ").grid(row=0, column=0)
         tk.Entry(ts_frame).grid(row=0, column=1)
         ex_frame: tk.Frame = tk.Frame(topframe)
         topframe.rowconfigure(1, weight=1)
-        ex_frame.grid(row=1, column=1, sticky=tk.EW)
+        ex_frame.grid(row=1, column=0, sticky=tk.EW)
         btn_frame: tk.Frame = tk.Frame(topframe)
         btn_frame.grid(row=2, column=0, columnspan=2, sticky=tk.EW)
         tk.Button(btn_frame, text="Add exercise").grid(row=0, column=0)
         tk.Button(btn_frame, text="Save workout").grid(row=0, column=1)
-        # self.add_exercise(ex_frame)
+        self.add_exercise(ex_frame)
 
     def _on_closing(self):
         if self._root:
@@ -36,23 +36,23 @@ class RoutineEditor(tk.Toplevel):
         if ex_frame in self.wo_exercises:
             del self.wo_exercises[ex_frame]
 
-    def add_exercise(self, ex_box: tk.Frame) -> None:
+    def add_exercise(self, ex_box: tk.Frame, start_row: int = 1) -> None:
         wo_set: list[tk.Entry] = []
 
         ex_frame: tk.Frame = tk.Frame(ex_box)
-        ex_frame.grid(sticky=tk.EW)
-        ttk.Combobox(ex_frame).grid(column=0, sticky=tk.W)
+        ex_frame.grid(row=start_row, column=0, sticky=tk.EW)
+        ttk.Combobox(ex_frame).grid(row=0, column=0, sticky=tk.W)
         weight = tk.Entry(ex_frame)
         wo_set.append(weight)
-        weight.grid(column=1, sticky=tk.W)
+        weight.grid(row=0, column=1, sticky=tk.W)
         reps = tk.Entry(ex_frame)
         wo_set.append(reps)
         self.wo_exercises[ex_frame] = wo_set
-        reps.grid(column=2, sticky=tk.W)
+        reps.grid(row=0, column=2, sticky=tk.W)
         del_btn = tk.Button(
             ex_frame, text="Delete", command=lambda: self.remove_exerecise(ex_frame)
         )
-        del_btn.grid(column=3, sticky=tk.W)
+        del_btn.grid(row=0, column=3, sticky=tk.W)
 
 
 if __name__ == "__main__":
