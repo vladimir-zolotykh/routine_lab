378e07eab1a4275e600047662d0a56c060999f23
Vladimir Zolotykh <vladimir.zolotykh@gmail.com>
2025-08-16 20:37:01 +0300

    Initial import
    Copied model.py from workout_lab. database is new, has only
    exercise_names (list[str])

---
+++ database.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# PYTHON_ARGCOMPLETE_OK
+
+
+exercise_names: list[str] = [
+    "front squat",
+    "squat",
+    "bench press",
+    "deadlift",
+    "pullup",
+    "overhead press",
+    "biceps curl",
+]
