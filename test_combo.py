cc72992ec19c1fdf42b0be235e785aa6be5e9954
Vladimir Zolotykh <vladimir.zolotykh@gmail.com>
2025-08-17 21:03:07 +0300

    Add _make_var, _print_str_var

--- test_combo.py
+++ test_combo.py
@@ -26,12 +26,32 @@ class TestCombo(tk.Tk):
         name_var = tk.StringVar(value="front squat")
         menu.add_command(label="Print name_var", command=lambda: print(name_var.get()))
         menu.add_command(label="Add Combobox", command=self.add_combobox)
+        menu.add_command(
+            label="Print 'str_var_' attributes", command=self._print_str_var
+        )
         cb = ttk.Combobox(self.box, textvariable=name_var, values=self.ex_names)
         cb.grid()
 
+        self.str_num: int = 1
+        self.str_var_dict: dict[str, tk.StringVar] = {}
+
+    def _print_str_var(self):
+        print(f"{self.str_var_dict = }")
+
+    def _make_var(self, value: str, prefix: str = "str_var_") -> tk.StringVar:
+        var_name = prefix + str(self.str_num)
+        self.str_num += 1
+        var = tk.StringVar(value=value)
+        setattr(self, var_name, var)
+        self.str_var_dict[var_name] = var
+        return var
+
     def add_combobox(self):
-        self.name_var2 = tk.StringVar(value=self.ex_names[0])
-        cb = ttk.Combobox(self.box, textvariable=self.name_var2, values=self.ex_names)
+        cb = ttk.Combobox(
+            self.box,
+            textvariable=self._make_var(self.ex_names[0]),
+            values=self.ex_names,
+        )
         cb.grid()
 
 
